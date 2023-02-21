8ede7e04488d93b7f4b173f75cb5bc512443c64f
vbetsch <vic.betsch@gmail.com>
2023-02-21 13:01:18 +0100

    🔥 Delete useless component

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,12 +1,11 @@
 import React from "react"
-import Submit from "./Submit";
 import Input from "./Input";
 
 const Form: React.FC = () => (
     <form className="form" action="">
         <Input rows="1" placeholder="Write a title..." />
         <Input rows="5" placeholder="Write a note..." />
-        <Submit value="Add note" />
+        <button className="button" type="submit">Add a note</button>
     </form>
 );
 
